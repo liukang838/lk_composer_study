10bbabc79029921c024f26608506d1f8ff799b09
qingxu <qingxu@ju-inc.com>
2018-11-16 00:02:39 +0800

    fx

---
+++ src/Content/Start/Study.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liukang
+ * Date: 2018/11/15
+ * Time: 下午11:55
+ */
+
+namespace Content\Start;
+
+
+class study
+{
+    public function __construct()
+    {
+    }
+
+    public function index()
+    {
+        echo "hello composer!";
+    }
+}
